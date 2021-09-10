171b8fbc400a54c4e2121e62f0c099c1e3238cff
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-10 10:49:29 -0700

    add navbar to app

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function NavBar()  {
+  return(
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#!">Blazer Merch</a>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-md-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#!">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#!">Link</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar;

54987d2d516c810a041e23f6d20d7d5b8843ddd7
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-10 11:11:11 -0700

    add KegList to app

---
+++ src/components/KegList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function KegList(props) {
+  return (
+    <>
+      {props.kegList.map((keg) =>
+        <Keg 
+           whenKegClicked = { props.onKegSelection }
+           name={keg.name}
+           brand={keg.brand}
+           price={keg.price}
+           alcoholContent={keg.alcoholContent}
+           id={keg.id}
+           key={keg.id}/> 
+      )}
+    </>
+  )
+}
+
+export default KegList;
